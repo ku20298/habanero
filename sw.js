850d431e25f1d149823fabd520bd100d3d98cccd
ku20298 <31235499+ku20298@users.noreply.github.com>
2021-03-22 17:16:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,13 +8,13 @@ var urlsToCache = [
 
 // インストール処理
 self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches
-			.open(CACHE_NAME)
-			.then(function(cache) {
-				return cache.addAll(urlsToCache);
-			})
-	);
+	// event.waitUntil(
+	// 	caches
+	// 		.open(CACHE_NAME)
+	// 		.then(function(cache) {
+	// 			return cache.addAll(urlsToCache);
+	// 		})
+	// );
 });
 
 // リソースフェッチ時のキャッシュロード処理
